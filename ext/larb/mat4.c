6d0e24f69991819298493b0e18bd6c10b77bc68c
ydah <t.yudai92@gmail.com>
2026-01-10 17:33:45 +0900

    Refactor mat4 functions for improved readability and performance

--- ext/larb/mat4.c
+++ ext/larb/mat4.c
@@ -52,15 +52,16 @@ static VALUE mat4_build16(VALUE klass, double v0, double v1, double v2,
   return mat4_build(klass, values);
 }
 
-static void vec3_normalize(double *x, double *y, double *z) {
+static inline void vec3_normalize(double *x, double *y, double *z) {
   double len = sqrt((*x) * (*x) + (*y) * (*y) + (*z) * (*z));
   *x /= len;
   *y /= len;
   *z /= len;
 }
 
-static void vec3_cross(double ax, double ay, double az, double bx, double by,
-                       double bz, double *rx, double *ry, double *rz) {
+static inline void vec3_cross(double ax, double ay, double az, double bx,
+                              double by, double bz, double *rx, double *ry,
+                              double *rz) {
   *rx = ay * bz - az * by;
   *ry = az * bx - ax * bz;
   *rz = ax * by - ay * bx;
@@ -377,62 +378,62 @@ VALUE mat4_transpose(VALUE self) {
 
 VALUE mat4_inverse(VALUE self) {
   Mat4Data *a = mat4_get(self);
-  double *m = a->data;
+  const double *m = a->data;
+  const double m0 = m[0];
+  const double m1 = m[1];
+  const double m2 = m[2];
+  const double m3 = m[3];
+  const double m4 = m[4];
+  const double m5 = m[5];
+  const double m6 = m[6];
+  const double m7 = m[7];
+  const double m8 = m[8];
+  const double m9 = m[9];
+  const double m10 = m[10];
+  const double m11 = m[11];
+  const double m12 = m[12];
+  const double m13 = m[13];
+  const double m14 = m[14];
+  const double m15 = m[15];
   double inv[16];
 
-  inv[0] = m[5] * m[10] * m[15] - m[5] * m[11] * m[14] -
-           m[9] * m[6] * m[15] + m[9] * m[7] * m[14] +
-           m[13] * m[6] * m[11] - m[13] * m[7] * m[10];
-  inv[4] = -m[4] * m[10] * m[15] + m[4] * m[11] * m[14] +
-           m[8] * m[6] * m[15] - m[8] * m[7] * m[14] -
-           m[12] * m[6] * m[11] + m[12] * m[7] * m[10];
-  inv[8] = m[4] * m[9] * m[15] - m[4] * m[11] * m[13] -
-           m[8] * m[5] * m[15] + m[8] * m[7] * m[13] +
-           m[12] * m[5] * m[11] - m[12] * m[7] * m[9];
-  inv[12] = -m[4] * m[9] * m[14] + m[4] * m[10] * m[13] +
-            m[8] * m[5] * m[14] - m[8] * m[6] * m[13] -
-            m[12] * m[5] * m[10] + m[12] * m[6] * m[9];
+  inv[0] = m5 * m10 * m15 - m5 * m11 * m14 - m9 * m6 * m15 +
+           m9 * m7 * m14 + m13 * m6 * m11 - m13 * m7 * m10;
+  inv[4] = -m4 * m10 * m15 + m4 * m11 * m14 + m8 * m6 * m15 -
+           m8 * m7 * m14 - m12 * m6 * m11 + m12 * m7 * m10;
+  inv[8] = m4 * m9 * m15 - m4 * m11 * m13 - m8 * m5 * m15 +
+           m8 * m7 * m13 + m12 * m5 * m11 - m12 * m7 * m9;
+  inv[12] = -m4 * m9 * m14 + m4 * m10 * m13 + m8 * m5 * m14 -
+            m8 * m6 * m13 - m12 * m5 * m10 + m12 * m6 * m9;
 
-  inv[1] = -m[1] * m[10] * m[15] + m[1] * m[11] * m[14] +
-           m[9] * m[2] * m[15] - m[9] * m[3] * m[14] -
-           m[13] * m[2] * m[11] + m[13] * m[3] * m[10];
-  inv[5] = m[0] * m[10] * m[15] - m[0] * m[11] * m[14] -
-           m[8] * m[2] * m[15] + m[8] * m[3] * m[14] +
-           m[12] * m[2] * m[11] - m[12] * m[3] * m[10];
-  inv[9] = -m[0] * m[9] * m[15] + m[0] * m[11] * m[13] +
-           m[8] * m[1] * m[15] - m[8] * m[3] * m[13] -
-           m[12] * m[1] * m[11] + m[12] * m[3] * m[9];
-  inv[13] = m[0] * m[9] * m[14] - m[0] * m[10] * m[13] -
-            m[8] * m[1] * m[14] + m[8] * m[2] * m[13] +
-            m[12] * m[1] * m[10] - m[12] * m[2] * m[9];
+  inv[1] = -m1 * m10 * m15 + m1 * m11 * m14 + m9 * m2 * m15 -
+           m9 * m3 * m14 - m13 * m2 * m11 + m13 * m3 * m10;
+  inv[5] = m0 * m10 * m15 - m0 * m11 * m14 - m8 * m2 * m15 +
+           m8 * m3 * m14 + m12 * m2 * m11 - m12 * m3 * m10;
+  inv[9] = -m0 * m9 * m15 + m0 * m11 * m13 + m8 * m1 * m15 -
+           m8 * m3 * m13 - m12 * m1 * m11 + m12 * m3 * m9;
+  inv[13] = m0 * m9 * m14 - m0 * m10 * m13 - m8 * m1 * m14 +
+            m8 * m2 * m13 + m12 * m1 * m10 - m12 * m2 * m9;
 
-  inv[2] = m[1] * m[6] * m[15] - m[1] * m[7] * m[14] -
-           m[5] * m[2] * m[15] + m[5] * m[3] * m[14] +
-           m[13] * m[2] * m[7] - m[13] * m[3] * m[6];
-  inv[6] = -m[0] * m[6] * m[15] + m[0] * m[7] * m[14] +
-           m[4] * m[2] * m[15] - m[4] * m[3] * m[14] -
-           m[12] * m[2] * m[7] + m[12] * m[3] * m[6];
-  inv[10] = m[0] * m[5] * m[15] - m[0] * m[7] * m[13] -
-            m[4] * m[1] * m[15] + m[4] * m[3] * m[13] +
-            m[12] * m[1] * m[7] - m[12] * m[3] * m[5];
-  inv[14] = -m[0] * m[5] * m[14] + m[0] * m[6] * m[13] +
-            m[4] * m[1] * m[14] - m[4] * m[2] * m[13] -
-            m[12] * m[1] * m[6] + m[12] * m[2] * m[5];
+  inv[2] = m1 * m6 * m15 - m1 * m7 * m14 - m5 * m2 * m15 +
+           m5 * m3 * m14 + m13 * m2 * m7 - m13 * m3 * m6;
+  inv[6] = -m0 * m6 * m15 + m0 * m7 * m14 + m4 * m2 * m15 -
+           m4 * m3 * m14 - m12 * m2 * m7 + m12 * m3 * m6;
+  inv[10] = m0 * m5 * m15 - m0 * m7 * m13 - m4 * m1 * m15 +
+            m4 * m3 * m13 + m12 * m1 * m7 - m12 * m3 * m5;
+  inv[14] = -m0 * m5 * m14 + m0 * m6 * m13 + m4 * m1 * m14 -
+            m4 * m2 * m13 - m12 * m1 * m6 + m12 * m2 * m5;
 
-  inv[3] = -m[1] * m[6] * m[11] + m[1] * m[7] * m[10] +
-           m[5] * m[2] * m[11] - m[5] * m[3] * m[10] -
-           m[9] * m[2] * m[7] + m[9] * m[3] * m[6];
-  inv[7] = m[0] * m[6] * m[11] - m[0] * m[7] * m[10] -
-           m[4] * m[2] * m[11] + m[4] * m[3] * m[10] +
-           m[8] * m[2] * m[7] - m[8] * m[3] * m[6];
-  inv[11] = -m[0] * m[5] * m[11] + m[0] * m[7] * m[9] +
-            m[4] * m[1] * m[11] - m[4] * m[3] * m[9] -
-            m[8] * m[1] * m[7] + m[8] * m[3] * m[5];
-  inv[15] = m[0] * m[5] * m[10] - m[0] * m[6] * m[9] -
-            m[4] * m[1] * m[10] + m[4] * m[2] * m[9] +
-            m[8] * m[1] * m[6] - m[8] * m[2] * m[5];
+  inv[3] = -m1 * m6 * m11 + m1 * m7 * m10 + m5 * m2 * m11 -
+           m5 * m3 * m10 - m9 * m2 * m7 + m9 * m3 * m6;
+  inv[7] = m0 * m6 * m11 - m0 * m7 * m10 - m4 * m2 * m11 +
+           m4 * m3 * m10 + m8 * m2 * m7 - m8 * m3 * m6;
+  inv[11] = -m0 * m5 * m11 + m0 * m7 * m9 + m4 * m1 * m11 -
+            m4 * m3 * m9 - m8 * m1 * m7 + m8 * m3 * m5;
+  inv[15] = m0 * m5 * m10 - m0 * m6 * m9 - m4 * m1 * m10 +
+            m4 * m2 * m9 + m8 * m1 * m6 - m8 * m2 * m5;
 
-  double det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+  double det = m0 * inv[0] + m1 * inv[4] + m2 * inv[8] + m3 * inv[12];
   if (fabs(det) < 1e-10) {
     rb_raise(rb_eRuntimeError, "Matrix is not invertible");
   }
